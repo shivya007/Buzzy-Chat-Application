cbe5f558d04864c0f06701b6541e98d4356f4436
Shiv Arora <passionshiv007@gmail.com>
2025-02-08 23:07:27 +0530

    Buzzy Chat Application

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: "all", // ✅ Fix: Use a string "all" instead of all
+  },
 }
 
